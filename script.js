546e2501d8783e91a516f68153fda0bcc236c91d
Sam Page <samnpage96@gmail.com>
2024-05-01 16:46:41 -0400

    Initialized window.onload function.

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const container = document.getElementById('container');
+
+    const createLabel = (name) => {
+        const label = document.createElement('div');
+        label.className = 'label';
+        label.textContent = name;
+        container.appendChild(label)
+    }
+}
